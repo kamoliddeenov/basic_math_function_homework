83e8e4e3c9dff450c064f1fc984b46993043e4fc
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-24 09:57:56 +0500

    19-misol

--- basic_func19.py
+++ basic_func19.py
@@ -1,7 +1,4 @@
-# Create a function called main.
-# Create function arguments a and b.
-# Return the absolute value of the difference between a and b.
+def main(a,b):
+    return abs(a-b)
 
-# Example:
-# Input: a = 4 b = 11
-# Output: 7
+print(main(4,11))
