45d6ccf92d074424c8533119252c1fbaefb603ac
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-24 09:56:03 +0500

    16-misol

--- basic_func16.py
+++ basic_func16.py
@@ -1,7 +1,4 @@
-# Create a function called main.
-# Create function arguments a and b.
-# Returns the sum of a and b.
+def main(a,b):
+    return a+b
 
-# Example:
-# Input: a = 3 b = 2
-# Output: 5
+print(main(3,2))
