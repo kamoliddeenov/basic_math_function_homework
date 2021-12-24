b889ff5d6ecb9f3fc3c1b77c4ef0e551c4b8d931
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-24 09:58:27 +0500

    20-misol

--- basic_func20.py
+++ basic_func20.py
@@ -1,7 +1,4 @@
-# Create a function called main.
-# Create function arguments a and b.
-# Return the multiplication of a and b.
+def main(a,b):
+    return a*b
 
-# Example:
-# Input: a = 5 b = 4
-# Output: 20
+print(main(5,4))
