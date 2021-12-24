f221a07bb9c66c9ddf5d72bf2f377c131a9184be
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-24 09:57:16 +0500

    18-misol

--- basic_func18.py
+++ basic_func18.py
@@ -1,8 +1,5 @@
-# Create a function called main.
-# Create function arguments a.
-# Assign the value pi to the variable "a" and return.
-# Round the result to 2 decimal places.
+from math import pi
+def main(a):
+    return round(a,2)
 
-# Example:
-# Input: a = pi
-# Output: 3.14
+print(main(pi))
