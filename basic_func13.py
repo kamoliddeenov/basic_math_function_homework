fc95e28bf8cf41b2f9f3071e6128fc287b7f7d33
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-24 09:54:56 +0500

    13-misol

--- basic_func13.py
+++ basic_func13.py
@@ -1,7 +1,5 @@
-# Create a function called main.
-# Create function argument a.
-# Returns the square root of a number.
+from math import sqrt
+def main(a):
+    return sqrt(a)
 
-# Example:
-# Input: a = 9
-# Output: 3.0
+print(main(9))
