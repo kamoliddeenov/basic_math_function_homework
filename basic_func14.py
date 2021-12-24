0c9ce788712c5db7e6c2c40451a0155f430564a3
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-24 09:55:15 +0500

    14-misol

--- basic_func14.py
+++ basic_func14.py
@@ -1,7 +1,4 @@
-# Create a function called main.
-# Create function arguments a and b.
-# Returns the floor division of a and b.
+def main(a,b):
+    return a//b
 
-# Example:
-# Input: a = 11 b = 2
-# Output: 5
+print(main(11,2))
