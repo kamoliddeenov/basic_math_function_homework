d7a330c36cb0cfbe9ec6d8a20d77d67cd5e95436
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-24 09:55:39 +0500

    15-misol

--- basic_func15.py
+++ basic_func15.py
@@ -1,7 +1,4 @@
-# Create a function called main.
-# Create function arguments a.
-# returns the modulus of a and b.
+def main(a,b):
+    return a%b
 
-# Example:
-# Input: a = 23 b = 5
-# Output: 3
+print(main(23,5))
