106ddf1fd12e393c1c7c2ac386b877a162ade82c
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-24 09:54:19 +0500

    12-misol

--- basic_func12.py
+++ basic_func12.py
@@ -1,8 +1,4 @@
-# Create a function called main.
-# Create function argument a.
-# Return the value of a.
-# Round the result to 2 decimal places.
+def main(a):
+    return round(a, 2)
 
-# Example:
-# Input: a = 6.453
-# Output: 6.45
+print(main(6.453))
