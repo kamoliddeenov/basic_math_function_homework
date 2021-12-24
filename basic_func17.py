74f373910dc8b93cc02fcf4167892a13f383f89a
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-24 09:56:40 +0500

    17-misol

--- basic_func17.py
+++ basic_func17.py
@@ -1,7 +1,5 @@
-# Create a function called main.
-# Create function arguments a.
-# Assign the value pi to the variable "a" and return.
+from math import pi
+def main(a):
+    return a
 
-# Example:
-# Input: a = pi
-# Output: pi
+print(main(pi))
